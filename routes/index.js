66f008f9509bdcb3576d7b842ff2fa6fec511b30
Nikola Selic <selich.work@gmail.com>
2019-10-26 09:03:17 +0200

    camera works

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+
+data = ""
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -9,7 +11,7 @@ router.get('/', function(req, res, next) {
 /* GET home page. */
 router.post('/barcode', function(req, res, next) {
   console.log(JSON.parse(res))
-  res.write("works");
+  data = JSON.parse(res)
 });
 
 module.exports = router;
