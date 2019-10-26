fed0d352f9d030dd968f15eda509cf6f086070fa
Velibor <veliborvasiljevic7@gmail.com>
2019-10-26 12:22:51 +0200

    shops.js works, sorting from the closest

--- routes/shops.js
+++ routes/shops.js
@@ -1,28 +1,5 @@
 var express = require('express');
-var request = require('request');
 var router = express.Router();
-
-var productService = require("../services/productService")
-
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    console.log("data")
-    res.render('index', {
-        title: 'Express'
-    });
-});
-
-/* GET home page. */
-// router.post('/barcode', function(req, res, next) {
-//   productService.newProduct(req,res)
-// });
-
-router.post('/products', function(req, res, next) {
-    console.log(res)
-
-    // productService.get(req,res)
-});
 
 
 // getting the closest tesco store
@@ -38,4 +15,5 @@ router.get('/shops', function(req, res, next) {
         res.send(body)
     })
 });
+
 module.exports = router;
